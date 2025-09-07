6ea4b3cb87ec7d00f94b39c8db65fb951fd5e4e1
NL <92334468+niko-logothetis@users.noreply.github.com>
2025-09-07 14:22:21 +0300

    Update config.py
    Added
    
    from pydantic_settings import BaseSettings, SettingsConfigDict

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 from typing import Optional, List
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict 
 
 
 class Settings(BaseSettings):
